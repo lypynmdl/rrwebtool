180711d8199c278b24f479c29b98b1de608e66fb
lyp3117 <lyp3117@163.com>
2021-09-22 15:12:41 +0800

    fix: lint报错修复

--- src/main.js
+++ src/main.js
@@ -1,13 +1,11 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from 'vue';
+import App from './App.vue';
 import installElement from './element-plus';
-import 'element-plus/packages/theme-chalk/src/base.scss'
-import './assets/basic.scss'
-import router from './router/index'
+import 'element-plus/packages/theme-chalk/src/base.scss';
+import './assets/basic.scss';
+import router from './router/index';
 
-
-
-const app = createApp(App)
-installElement(app)
-app.use(router)
-app.mount('#app')
+const app = createApp(App);
+installElement(app);
+app.use(router);
+app.mount('#app');
